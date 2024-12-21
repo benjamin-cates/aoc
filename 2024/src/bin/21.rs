76b5041599c718a71a033dd608f14cc25f4b59a6
Benjamin Cates <thebenjicat@gmail.com>
2024-12-21 12:27:34 -0800

    Refactor day 21

--- 2024/src/bin/21.rs
+++ 2024/src/bin/21.rs
@@ -17,110 +17,68 @@ fn find_fastest(start: Point, end: Point, depth: usize, top: bool) -> usize {
     let right: Point = (2, 1).into();
     let up: Point = (1, 0).into();
     let down: Point = (1, 1).into();
-    let activate: Point = (2, 0).into();
+    let actv: Point = (2, 0).into();
     if depth == 0 {
         return (x.abs() + y.abs()) as usize + 1;
     }
     let invalid_spot: Point = if top { (0, 3).into() } else { (0, 0).into() };
-    let out = if x < 0 {
-        if y < 0 {
-            // Up and left
-            let path_1 = find_fastest(activate, up, depth - 1, false)
-                + find_fastest(up, left, depth - 1, false)
-                + find_fastest(left, activate, depth - 1, false)
-                + (y.abs() as usize - 1)
-                + (x.abs() as usize - 1);
-            if start.x + x == invalid_spot.x && start.y == invalid_spot.y {
-                path_1
-            } else {
-                let path_2 = find_fastest(activate, left, depth - 1, false)
-                    + find_fastest(left, up, depth - 1, false)
-                    + find_fastest(up, activate, depth - 1, false)
-                    + (y.abs() as usize - 1)
-                    + (x.abs() as usize - 1);
-                path_1.min(path_2)
-            }
-        } else if y == 0 {
-            // Left
-            let out = find_fastest(activate, left, depth - 1, false)
-                + find_fastest(left, activate, depth - 1, false)
-                + (x.abs() as usize - 1);
-            out
+    if x == 0 && y == 0 {
+        return 1;
+    }
+    let path = |from: Point, to: Point| find_fastest(from, to, depth - 1, false);
+    let two_loop = |a: Point, b: Point| path(a, b) + path(b, a);
+    if y == 0 && x < 0 {
+        // Left
+        return two_loop(actv, left) + x.abs() as usize - 1;
+    }
+    if y == 0 && x > 0 {
+        // Rigth
+        return two_loop(actv, right) + x.abs() as usize - 1;
+    }
+    if x == 0 && y < 0 {
+        // Up
+        return two_loop(actv, up) + y.abs() as usize - 1;
+    }
+    if x == 0 && y > 0 {
+        // Down
+        return two_loop(actv, down) + y.abs() as usize - 1;
+    }
+    let three_loop = |a: Point, b: Point, c: Point| path(a, b) + path(b, c) + path(c, a);
+    let y_presses = y.abs() as usize - 1;
+    let x_presses = x.abs() as usize - 1;
+    if x < 0 && y < 0 {
+        // Up and left
+        return if start + (x, 0).into() == invalid_spot {
+            three_loop(actv, up, left) + x_presses + y_presses
         } else {
-            // Down and left
-            let path_1 = find_fastest(activate, down, depth - 1, false)
-                + find_fastest(down, left, depth - 1, false)
-                + find_fastest(left, activate, depth - 1, false)
-                + (y.abs() as usize - 1)
-                + (x.abs() as usize - 1);
-            if start.x + x == invalid_spot.x && start.y == invalid_spot.y {
-                path_1
-            } else {
-                let path_2 = find_fastest(activate, left, depth - 1, false)
-                    + (y.abs() as usize - 1)
-                    + find_fastest(left, down, depth - 1, false)
-                    + (x.abs() as usize - 1)
-                    + find_fastest(down, activate, depth - 1, false);
-                path_1.min(path_2)
-            }
-        }
-    } else if x == 0 {
-        if y < 0 {
-            // Up
-            find_fastest(activate, up, depth - 1, false)
-                + (y.abs() as usize - 1)
-                + find_fastest(up, activate, depth - 1, false)
-        } else if y == 0 {
-            1
+            three_loop(actv, up, left).min(three_loop(actv, left, up)) + x_presses + y_presses
+        };
+    }
+    if x < 0 && y > 0 {
+        // Down and left
+        return if start + (x, 0).into() == invalid_spot {
+            three_loop(actv, down, left) + x_presses + y_presses
         } else {
-            // Down
-            find_fastest(activate, down, depth - 1, false)
-                + (y.abs() as usize - 1)
-                + find_fastest(down, activate, depth - 1, false)
-        }
-    } else {
-        if y < 0 {
-            // Up and right
-            let path_2 = find_fastest(activate, right, depth - 1, false)
-                + (y.abs() as usize - 1)
-                + find_fastest(right, up, depth - 1, false)
-                + (x.abs() as usize - 1)
-                + find_fastest(up, activate, depth - 1, false);
-            if start.x == invalid_spot.x && start.y + y == invalid_spot.y {
-                path_2
-            } else {
-                let path_1 = find_fastest(activate, up, depth - 1, false)
-                    + (y.abs() as usize - 1)
-                    + find_fastest(up, right, depth - 1, false)
-                    + (x.abs() as usize - 1)
-                    + find_fastest(right, activate, depth - 1, false);
-                path_1.min(path_2)
-            }
-        } else if y == 0 {
-            // Right
-            find_fastest(activate, right, depth - 1, false)
-                + (x.abs() as usize - 1)
-                + find_fastest(right, activate, depth - 1, false)
+            three_loop(actv, left, down).min(three_loop(actv, left, down)) + x_presses + y_presses
+        };
+    }
+    if x > 0 && y < 0 {
+        // Up and right
+        return if start + (0, y).into() == invalid_spot {
+            three_loop(actv, right, up) + x_presses + y_presses
         } else {
-            // Down and right
-            let path_2 = find_fastest(activate, right, depth - 1, false)
-                + (y.abs() as usize - 1)
-                + find_fastest(right, down, depth - 1, false)
-                + (x.abs() as usize - 1)
-                + find_fastest(down, activate, depth - 1, false);
-            if start.y + y == invalid_spot.y && start.x == invalid_spot.x {
-                path_2
-            } else {
-                let path_1 = find_fastest(activate, down, depth - 1, false)
-                    + (y.abs() as usize - 1)
-                    + find_fastest(down, right, depth - 1, false)
-                    + (x.abs() as usize - 1)
-                    + find_fastest(right, activate, depth - 1, false);
-                path_1.min(path_2)
-            }
-        }
-    };
-    out
+            three_loop(actv, right, up).min(three_loop(actv, up, right)) + x_presses + y_presses
+        };
+    }
+    if x > 0 && y > 0 {
+        // Down and right
+        return if start + (0, y).into() == invalid_spot {
+            three_loop(actv, right, down) + x_presses + y_presses
+        } else {
+            three_loop(actv, right, down).min(three_loop(actv, down, right)) + x_presses + y_presses
+        };
+    }
+    unreachable!()
 }
 
 // Finished in 03:45:23
